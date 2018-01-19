f60689011d7b6aeba22ee37806ae05be62f16ddb
nwyll <nataliewyll@yahoo.com>
2018-01-19 17:53:48 -0600

    rails g scaffold Post body:text topic:references

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :book_clubs do
     resources :topics, except: [:index]
   end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    body "MyText"
+    topic nil
+  end
+end
