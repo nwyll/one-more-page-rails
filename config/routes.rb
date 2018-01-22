d15cd6385dd427a047b7bf40a9fec9de31c2a4f2
nwyll <nataliewyll@yahoo.com>
2018-01-22 16:17:37 -0600

    rails g controller Comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts, only: [] do
-    resources :comments, only: [:update, :create, :destroy]
+    resources :comments, only: [:create, :update, :destroy]
   end
 
   root 'welcome#index'
