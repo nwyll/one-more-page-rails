89c1d900fe3055ace83fd9bd751e04a9795cd2d4
nwyll <nataliewyll@yahoo.com>
2018-01-23 17:55:31 -0600

    comment edit & delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: [:edit, :create, :update, :destroy]
+  before_action :set_post
   before_action :set_comment, only: [:edit, :update, :destroy]
 
   # GET posts/1/comments/1/edit
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
   # POST posts/1/comments
   # POST posts/1/comments.json
   def create
-    @comment = @post.comments.new(comment_params)
+    @comment = @post.comments.build(comment_params)
 
     respond_to do |format|
       if @comment.save
@@ -26,12 +26,12 @@ class CommentsController < ApplicationController
   # PATCH/PUT posts/1/comments/1.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to [@topic, @post], notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+      if @comment.update(comment_params)
+        format.html { redirect_to [@post.topic, @post], notice: 'Commment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,12 +53,12 @@ class CommentsController < ApplicationController
     end
 
     def set_comment
-      @comment = @post.comments.find(params[:comment_id])
+      @comment = @post.comments.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-     params.require(:comment).permit(:body, :post_id)
+     params.require(:comment).permit(:body)
    end
 
 end
