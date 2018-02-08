202b948d0841d9a557b1f6db519270667a727f68
nwyll <nataliewyll@yahoo.com>
2018-02-07 23:13:48 -0600

    memberships_controller_spec passing

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe MembershipsController, type: :controller do
   let(:member) { create(:user) }
   let(:admin) { create(:user, name: "Admin", email: "admin@bloc.io", role: :admin) }
   let(:my_book_club) { create(:book_club) }
+  let(:my_membership) {create(:membership, user: member, book_club: my_book_club)}
 
   context 'guest' do
     describe 'POST create' do
@@ -15,8 +16,7 @@ RSpec.describe MembershipsController, type: :controller do
 
     describe 'DELETE destroy' do
       it 'redirects the user to the sign in view' do
-        membership = member.memberships.create(book_club_id: my_book_club.id)
-        delete :destroy, params: { book_club: my_book_club.id, id: membership.id }
+        delete :destroy, params: { book_club_id: my_book_club.id, id: my_membership.id }
         expect(response).to redirect_to new_user_session_path
       end
     end
@@ -43,8 +43,7 @@ RSpec.describe MembershipsController, type: :controller do
 
     describe 'DELETE destroy' do
       it 'redirects to the book_club show view' do
-        membership = member.memberships.create(book_club_id: my_book_club.id)
-        delete :destroy, params: { book_club: my_book_club.id, id: membership.id }
+        delete :destroy, params: { book_club_id: my_book_club.id, id: my_membership.id }
         expect(response).to redirect_to(my_book_club)
       end
 
@@ -52,7 +51,7 @@ RSpec.describe MembershipsController, type: :controller do
         membership = member.memberships.create(book_club_id: my_book_club.id)
         expect(member.memberships.find_by_book_club_id(my_book_club.id)).not_to be_nil
 
-        delete :destroy, params: { book_club: my_book_club.id, id: membership.id }
+        delete :destroy, params: { book_club_id: my_book_club.id, id: membership.id }
         expect(member.memberships.find_by_book_club_id(my_book_club.id)).to be_nil
       end
     end
